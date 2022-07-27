28f92b44f2aa4a13c09008bb3973717acf405c74
Dmsrmdni <dimasramdani220@gmail.com>
2022-07-27 16:45:05 +0700

    Menambahkan Authentikasi

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Membuat sample admin
+        $admin = new \App\Models\User();
+        $admin->name = "Admin";
+        $admin->email = "admin@gmail.com";
+        $admin->password = bcrypt('admin123');
+        $admin->save();
+
+    }
+}
