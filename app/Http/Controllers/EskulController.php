fcdef11c4fe4fd912579606e3ced1181a0594dde
Dmsrmdni <dimasramdani220@gmail.com>
2022-07-28 23:32:01 +0700

    Menambahkan EskulController

--- app/Http/Controllers/EskulController.php
+++ app/Http/Controllers/EskulController.php
@@ -14,7 +14,8 @@ class EskulController extends Controller
      */
     public function index()
     {
-        //
+        $eskul = Eskul::all();
+        return view('admin.eskul.index', compact('eskul'));
     }
 
     /**
@@ -24,7 +25,7 @@ class EskulController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.eskul.create');
     }
 
     /**
@@ -35,7 +36,29 @@ class EskulController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validasi
+        $validated = $request->validate([
+            'nama_eskul' => 'required|unique:jurusans|max:255',
+            'deskripsi' => 'required',
+            'image' => 'required',
+        ]);
+
+        $eskul = new Eskul();
+        $eskul->nama_eskul = $request->nama_eskul;
+        $eskul->deskripsi = $request->deskripsi;
+        if ($request->hasFile('image')) {
+            $eskul->deleteImage(); //menghapus image sebelum di update melalui method deleteImage di model
+            $image = $request->file('image');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('images/eskul/', $name);
+            $eskul->image = $name;
+        }
+
+        $jurusan->save();
+        return redirect()
+            ->route('jurusan.index')
+            ->with('success', 'Data berhasil di Buat!');
+
     }
 
     /**
@@ -44,9 +67,10 @@ class EskulController extends Controller
      * @param  \App\Models\Eskul  $eskul
      * @return \Illuminate\Http\Response
      */
-    public function show(Eskul $eskul)
+    public function show($id)
     {
-        //
+        $eskul = Eskul::findOrFail($id);
+        return view('admin.eskul.show', compact('eskul'));
     }
 
     /**
@@ -55,9 +79,10 @@ class EskulController extends Controller
      * @param  \App\Models\Eskul  $eskul
      * @return \Illuminate\Http\Response
      */
-    public function edit(Eskul $eskul)
+    public function edit($id)
     {
-        //
+        $eskul = Eskul::findOrFail($id);
+        return view('admin.eskul.edit', compact('eskul'));
     }
 
     /**
@@ -67,9 +92,30 @@ class EskulController extends Controller
      * @param  \App\Models\Eskul  $eskul
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Eskul $eskul)
+    public function update(Request $request, $id)
     {
-        //
+        //validasi
+        $validated = $request->validate([
+            'nama_eskul' => 'required',
+            'deskripsi' => 'required',
+            'image' => 'required',
+        ]);
+
+        $eskul = Eskul::findOrFail($id);
+        $eskul->nama_eskul = $request->nama_eskul;
+        $eskul->deskripsi = $request->deskripsi;
+        if ($request->hasFile('image')) {
+            $eskul->deleteImage(); //menghapus image sebelum di update melalui method deleteImage di model
+            $image = $request->file('image');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('images/eskul/', $name);
+            $eskul->image = $name;
+        }
+
+        $eskul->save();
+        return redirect()
+            ->route('admin.eskul.index')
+            ->with('success', 'Data berhasil di Edit!');
     }
 
     /**
@@ -78,8 +124,14 @@ class EskulController extends Controller
      * @param  \App\Models\Eskul  $eskul
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Eskul $eskul)
+    public function destroy($id)
     {
-        //
+        $eskul = Eskul::findOrFail($id);
+        $eskul->deleteImage();
+        $eskul->delete();
+        return redirect()
+            ->route('admin.eskul.index')
+            ->with('success', 'Data berhasil di Hapus!');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EskulController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,10 @@ Route::get('/', function () {
 Auth::routes(['register' => false]); // Mematikan halaman register
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+    Route::resource('eskul', EskulController::class);
+});
