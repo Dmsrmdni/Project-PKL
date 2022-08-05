6800e6555f489dff10c271613e0c4de4a4480eb8
Dmsrmdni <dimasramdani220@gmail.com>
2022-08-05 08:18:01 +0700

    menambahkan icon bottom

--- resources/views/admin/galeri/edit.blade.php
+++ resources/views/admin/galeri/edit.blade.php
@@ -109,12 +109,18 @@
 
                 <!--end::Automation-->
                 <div class="d-flex justify-content-end">
-                    <!--begin::Button-->
-                    <a href="{{route('galeri.index')}}" class="btn btn-light me-5">Cancel</a>
-                    <!--end::Button-->
-                    <!--begin::Button-->
+                    <a href="{{route('eskul.index')}}" class="btn btn-danger me-3"><svg
+                            xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor"
+                            class="bi bi-skip-backward-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M.5 3.5A.5.5 0 0 0 0 4v8a.5.5 0 0 0 1 0V8.753l6.267 3.636c.54.313 1.233-.066 1.233-.697v-2.94l6.267 3.636c.54.314 1.233-.065 1.233-.696V4.308c0-.63-.693-1.01-1.233-.696L8.5 7.248v-2.94c0-.63-.692-1.01-1.233-.696L1 7.248V4a.5.5 0 0 0-.5-.5z" />
+                        </svg> Kembali</a>
                     <button type="submit" id="kt_ecommerce_add_category_submit" class="btn btn-primary">
-                        <span class="indicator-label">Save</span>
+                        <span class="indicator-label"><svg xmlns="http://www.w3.org/2000/svg" width="20"
+                                fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+                            </svg> Kirim </span>
                         <span class="indicator-progress">Please wait...
                             <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                     </button>
