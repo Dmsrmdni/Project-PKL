b56c9156808a98f160c4c7ffc0a1adc3a1ec5933
Dmsrmdni <dimasramdani220@gmail.com>
2022-08-08 13:58:51 +0700

    menamambahkan Tampilan

--- resources/views/admin/eskul/create.blade.php
+++ resources/views/admin/eskul/create.blade.php
@@ -15,7 +15,7 @@
             <div class="d-flex flex-column gap-7 gap-lg-10 w-5 w-lg-300px mb-7 me-lg-10">
                 <!--begin::Thumbnail settings-->
 
-                <div class="card card-flush py-4 shadow rounded card">
+                <div class="card card-flush py-4 shadow-lg rounded card">
                     <!--begin::Card header-->
                     <div class="card-header">
                         <!--begin::Card title-->
@@ -92,7 +92,7 @@
             <!--begin::Main column-->
             <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10">
                 <!--begin::General options-->
-                <div class="card card-flush py-4 shadow rounded card">
+                <div class="card card-flush py-4 shadow-lg rounded card">
                     <!--begin::Card header-->
                     <div class="card-header">
                         <div class="card-title">
